88c92a3d0b6ec3771fa9ebc8812d42fb074179bb
dbakhval <dbakhval@DBAKHVAL-MOBL>
2022-05-22 14:39:06 -0400

    Improved download speed for Windows

--- create_input.ps1
+++ create_input.ps1
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+$ProgressPreference = 'SilentlyContinue'
+
 wget http://dumps.wikimedia.org/huwikisource/latest/huwikisource-latest-pages-meta-current.xml.bz2 -OutFile huwikisource-latest-pages-meta-current.xml.bz2
 7z.exe x huwikisource-latest-pages-meta-current.xml.bz2
 mv huwikisource-latest-pages-meta-current.xml data/small.data
 
-wget http://dumps.wikimedia.org/huwiki/latest/huwiki-latest-pages-meta-current.xml.bz2 -OutFile huwiki-latest-pages-meta-current.xml.bz
+wget http://dumps.wikimedia.org/huwiki/latest/huwiki-latest-pages-meta-current.xml.bz2 -OutFile huwiki-latest-pages-meta-current.xml.bz2
 7z.exe x huwiki-latest-pages-meta-current.xml.bz2
 mv huwiki-latest-pages-meta-current.xml data/large.data
 
