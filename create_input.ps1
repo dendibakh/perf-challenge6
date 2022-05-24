62d87d2d5e4edd05febd0536b1026123482f38c3
dbakhval <dbakhval@DBAKHVAL-MOBL>
2022-05-23 20:51:43 -0400

    Fixed create_input.ps1

--- create_input.ps1
+++ create_input.ps1
@@ -2,13 +2,21 @@
 
 $ProgressPreference = 'SilentlyContinue'
 
+# 7zip gives Data Error while extracting large input file, thus using bzip2
+mkdir bzip2
+wget https://github.com/philr/bzip2-windows/releases/download/v1.0.8.0/bzip2-1.0.8.0-win-x64.zip -OutFile bzip2/bzip2-1.0.8.0-win-x64.zip
+7z.exe x bzip2/bzip2-1.0.8.0-win-x64.zip -obzip2
+
 wget http://dumps.wikimedia.org/huwikisource/latest/huwikisource-latest-pages-meta-current.xml.bz2 -OutFile huwikisource-latest-pages-meta-current.xml.bz2
-7z.exe x huwikisource-latest-pages-meta-current.xml.bz2
+bzip2/bzip2.exe -d huwikisource-latest-pages-meta-current.xml.bz2
 mv huwikisource-latest-pages-meta-current.xml data/small.data
+rm huwikisource-latest-pages-meta-current.xml.bz2
 
 wget http://dumps.wikimedia.org/huwiki/latest/huwiki-latest-pages-meta-current.xml.bz2 -OutFile huwiki-latest-pages-meta-current.xml.bz2
-7z.exe x huwiki-latest-pages-meta-current.xml.bz2
+bzip2/bzip2.exe -d huwiki-latest-pages-meta-current.xml.bz2
 mv huwiki-latest-pages-meta-current.xml data/large.data
+rm huwiki-latest-pages-meta-current.xml.bz2
 
-7z.exe x small-ref.bz2
+bzip2/bzip2.exe -d small-ref.bz2
 mv small-ref ref/small.out
+rm small-ref.bz2
