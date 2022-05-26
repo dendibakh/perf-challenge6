e69466ac5b447f91cf36909ceeaca9dc1869ddb3
dendibakh <dendibakh@gmail.com>
2022-05-26 08:41:17 -0700

    Run clang-format

--- wordcount.cpp
+++ wordcount.cpp
@@ -5,14 +5,17 @@
 #include <iostream>
 
 // Assumptions
-// 1. Function should read the input from the file, i.e. caching the input is not allowed.
+// 1. Function should read the input from the file, i.e. caching the input is
+// not allowed.
 // 2. The input is always encoded in UTF-8.
 // 3. Break only on space, tab and newline (do not break on non-breaking space).
 // 4. Sort words by frequency AND secondary sort in alphabetical order.
 
 // Implementation rules
-// 1. You can add new files but dependencies are generally not allowed unless it is a header-only library.
-// 2. Your submission must be single-threaded, however feel free to implement multi-threaded version (optional).
+// 1. You can add new files but dependencies are generally not allowed unless it
+// is a header-only library.
+// 2. Your submission must be single-threaded, however feel free to implement
+// multi-threaded version (optional).
 
 #ifdef SOLUTION
 //
