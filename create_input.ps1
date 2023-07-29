62ebb0a4667da938e94995bc5531f94dd118e00d
Denis Bakhvalov <dendibakh@gmail.com>
2023-07-29 12:11:13 -0400

    Updated mirror with inputs

--- create_input.ps1
+++ create_input.ps1
@@ -13,11 +13,11 @@ mkdir bzip2
 wget https://github.com/philr/bzip2-windows/releases/download/v1.0.8.0/bzip2-1.0.8.0-win-x64.zip -OutFile bzip2/bzip2-1.0.8.0-win-x64.zip
 7z.exe x bzip2/bzip2-1.0.8.0-win-x64.zip -obzip2
 
-wget https://dumps.wikimedia.org/huwikisource/$DUMPVERSION/huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2 -OutFile huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2
+wget https://dumps.wikimedia.your.org/huwikisource/$DUMPVERSION/huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2 -OutFile huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2
 bzip2/bzip2.exe -d huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2
 mv huwikisource-$DUMPVERSION-pages-meta-current.xml data/small.data
 
-wget https://dumps.wikimedia.org/huwiki/$DUMPVERSION/huwiki-$DUMPVERSION-pages-meta-current.xml.bz2 -OutFile huwiki-$DUMPVERSION-pages-meta-current.xml.bz2
+wget https://dumps.wikimedia.your.org/huwiki/$DUMPVERSION/huwiki-$DUMPVERSION-pages-meta-current.xml.bz2 -OutFile huwiki-$DUMPVERSION-pages-meta-current.xml.bz2
 bzip2/bzip2.exe -d huwiki-$DUMPVERSION-pages-meta-current.xml.bz2
 mv huwiki-$DUMPVERSION-pages-meta-current.xml data/large.data
 
--- create_input.sh
+++ create_input.sh
@@ -8,11 +8,11 @@ DUMPVERSION=20220520
 # https://meta.wikimedia.org/wiki/Data_dumps/Dump_frequency
 # DUMPVERSION=latest
 
-wget https://dumps.wikimedia.org/huwikisource/$DUMPVERSION/huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2
+wget https://dumps.wikimedia.your.org/huwikisource/$DUMPVERSION/huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2
 bunzip2 huwikisource-$DUMPVERSION-pages-meta-current.xml.bz2
 mv huwikisource-$DUMPVERSION-pages-meta-current.xml data/small.data
 
-wget https://dumps.wikimedia.org/huwiki/$DUMPVERSION/huwiki-$DUMPVERSION-pages-meta-current.xml.bz2
+wget https://dumps.wikimedia.your.org/huwiki/$DUMPVERSION/huwiki-$DUMPVERSION-pages-meta-current.xml.bz2
 bunzip2 huwiki-$DUMPVERSION-pages-meta-current.xml.bz2
 mv huwiki-$DUMPVERSION-pages-meta-current.xml data/large.data
 
